8cb5fc6d656771a3fbe1d61c89998a8aad783c6f
carl <carl@muchenglin.com>
2023-07-18 19:18:04 +0800

    b

--- MQ/simple/Consumer.go
+++ MQ/simple/Consumer.go
@@ -24,7 +24,7 @@ func main(){
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		"hello", // name
+		"simple-queue", // name
 		false,   // durable
 		false,   // delete when unused
 		false,   // exclusive
