742a3161ac45981363998e18bbff135d050fa114
carl <carl@muchenglin.com>
2023-07-29 17:17:39 +0800

    b

--- microservice-study/GoMicro/Greeter/client/main.go
+++ microservice-study/GoMicro/Greeter/client/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
 	"go-micro.dev/v4/client"
 	"go-micro.dev/v4/metadata"
 	"log"
@@ -30,6 +29,30 @@ func (t *traceWrapper) Call(ctx context.Context, req client.Request, rsp interfa
 	if !ok {
 		md = make(map[string]string)
 	}
+
+	span, ctx := opentracing.StartSpanFromContextWithTracer(ctx, opentracing.GlobalTracer(), req.Service()+"."+req.Endpoint())
+	defer span.Finish()
+
+	mdCarrier := opentracing.TextMapCarrier(md)
+	if err := opentracing.GlobalTracer().Inject(span.Context(), opentracing.TextMap, mdCarrier); err != nil {
+		return err
+	}
+
+	for k, v := range mdCarrier {
+		md[k] = v
+	}
+
+	log.Println(md)
+	ctx = metadata.NewContext(ctx, md)
+	return t.Client.Call(ctx, req, rsp, opts...)
+}
+
+/*
+func (t *traceWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		md = make(map[string]string)
+	}
 	spanCtx, _ := opentracing.GlobalTracer().Extract(opentracing.TextMap, opentracing.TextMapCarrier(md))
 	span := opentracing.StartSpan(req.Service()+"."+req.Endpoint(), ext.RPCServerOption(spanCtx))
 	defer span.Finish()
@@ -42,13 +65,13 @@ func (t *traceWrapper) Call(ctx context.Context, req client.Request, rsp interfa
 	for k, v := range mdCarrier {
 		md[k] = v
 	}
-
+	log.Println(md)
 	ctx = opentracing.ContextWithSpan(ctx, span)
 	ctx = metadata.NewContext(ctx, md)
 
 	return t.Client.Call(ctx, req, rsp, opts...)
 }
-/*
+
 func (t *traceWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
 	md, ok := metadata.FromContext(ctx)
 	if !ok {
